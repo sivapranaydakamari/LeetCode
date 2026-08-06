a2cfef97df0d7406ecbe3b0e10604db765315b63
DAKAMARI SIVA PRANAY <147487910+sivapranaydakamari@users.noreply.github.com>
2026-08-06 15:18:24 +0530

    Time: 22 ms (22.17%), Space: 45.6 MB (89.13%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        if(n <= 3) return new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < n; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1, right = n - 1;
+                while(left < right){
+                    long sum = (long) nums[i] + (long) nums[j] + (long) nums[left] + (long) nums[right];
+                    if(sum == target){
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while(left < right && nums[left] == nums[left + 1]) left++;
+                        while(left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    }else if(sum > target) right--;
+                    else left++;
+                }
+            }
+        }
+        return result;
+    }
+}
