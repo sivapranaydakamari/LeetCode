ae2b29456dab3fd0069f61c0cba81bfcd9f58de8
DAKAMARI SIVA PRANAY <147487910+sivapranaydakamari@users.noreply.github.com>
2026-08-07 14:30:26 +0530

    Time: 28 ms (34.89%), Space: 111.3 MB (18.94%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(nums.length == 1) return false;
-        int i = 0;
-        while(i < nums.length){
-            int j = Math.max(0, i-k);
-            while(j < i){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
-                j++;
-            }
-            i++;
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0; i < n; i++){
+            if(map.containsKey(nums[i]) && Math.abs(i - map.get(nums[i])) <= k)
+                return true;
+            map.put(nums[i], i); 
         }
         return false;
     }
