0b048c6bccc4bc0bbc84221ac574c21d62432772
DAKAMARI SIVA PRANAY <147487910+sivapranaydakamari@users.noreply.github.com>
2026-08-12 10:45:41 +0530

    Time: 26 ms (14.58%), Space: 48.4 MB (11.58%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,22 +1,23 @@
 class Solution {
-    private boolean isPossible(int speed, int[] piles, int h){
+    private boolean isPossible(int[] piles, int pile, int h){
+
         int hours = 0;
         for(int i = 0; i < piles.length; i++){
-            hours += Math.ceil((double) piles[i]/speed);
+            hours += Math.ceil((double) piles[i] / pile);
         }
         return hours <= h;
     }
+    
     public int minEatingSpeed(int[] piles, int h) {
         int max = piles[0];
         for(int i = 1; i < piles.length; i++){
-            max = Math.max(max, piles[i]);
+            max = Math.max(max, piles[i]);   
         }
-
         int low = 1, high = max;
         int ans = 0;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(isPossible(mid, piles, h)){
+            if(isPossible(piles, mid, h)){
                 ans = mid;
                 high = mid - 1;
             }else{
