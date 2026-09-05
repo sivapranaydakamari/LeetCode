44c002d68c2c499d00feb7b35edb16ee7a29e741
DAKAMARI SIVA PRANAY <147487910+sivapranaydakamari@users.noreply.github.com>
2026-09-05 11:19:07 +0530

    Time: 4 ms (88.55%), Space: 133.6 MB (5.34%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,20 +1,21 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int[] maxPrefix = new int[n];
-        maxPrefix[0] = nums[0];
-        for(int i = 1; i < n; i++){
-            maxPrefix[i] = Math.max(maxPrefix[i - 1], nums[i]);
-        }
+        // int[] maxPrefix = new int[n];
+        // maxPrefix[0] = nums[0];
+        // for(int i = 1; i < n; i++){
+        //     maxPrefix[i] = Math.max(maxPrefix[i - 1], nums[i]);
+        // }
 
         int[] minSuffix = new int[n];
         minSuffix[n - 1] = nums[n - 1];
         for(int i = n - 2; i >= 0; i--){
             minSuffix[i] = Math.min(minSuffix[i + 1], nums[i]);
         }
-
+        int maxSoFar = -1;
         for(int i = 0; i < n; i++){
-            int score = maxPrefix[i] - minSuffix[i];
+            maxSoFar = Math.max(maxSoFar, nums[i]);
+            int score = maxSoFar - minSuffix[i];
             if(score <= k){
                 return i;
             }
